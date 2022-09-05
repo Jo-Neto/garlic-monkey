fde674a4970926827a0094d6874d3605cd255fee
pedrolcsilva <pedrolucas443@gmail.com>
2022-09-05 10:46:43 -0300

    Make the SSL Certificate Trusted

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
+import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), mkcert()],
+  server: {
+    https: {
+      key: fs.readFileSync('./.certs/key.pem'),
+      cert: fs.readFileSync('./.certs/cert.pem')
+    },
+    hmr: {
+      host: 'localhost',
+    }
+  }
+})
