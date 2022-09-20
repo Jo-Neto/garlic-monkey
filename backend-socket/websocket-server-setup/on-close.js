6b66b47a7bdf4eac45878f5b6cb5a23f7f23c45e
Jo-Neto <jcb.oliveira.neto@gmail.com>
2022-09-20 18:20:08 -0300

    haaa

--- backend-socket/websocket-server-setup/on-close.js
+++ backend-socket/websocket-server-setup/on-close.js
@@ -16,6 +16,21 @@ module.exports = function onClose(ws) {
     if (activeSessionsArr[ws.sID].activeSockets.every( el => { el === null }) && activeSessionsArr[ws.sID].waitingSockets.every( el => { el === null })) //if session empty save on redis
         activeSessionsArr[ws.sID].saveOnDB(true);
     else { //player disconnected but there are still players
-        //TODO: wanr who disconnected
+        activeSessionsArr[ws.sID].activeSockets.forEach(webs => { //send new msg to all players in session
+            if (webs !== null && webs.readyState === 1) {
+                webs.send(JSON.stringify({
+                    msgType: 'playerUpdate',
+                    msgContent: { nick: ws.garlicName, updateType: 'out', isOnGame: ws.aID }
+                }));
+            }
+        });
+        activeSessionsArr[ws.sID].waitingSockets.forEach(webs => { //send new msg to all players in session
+            if (webs !== null && webs.readyState === 1) {
+                webs.send(JSON.stringify({
+                    msgType: 'playerUpdate',
+                    msgContent: { nick: ws.garlicName, updateType: 'out', isOnGame: ws.aID }
+                }));
+            }
+        });
     }
 };
