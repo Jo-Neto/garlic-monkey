feb7430d738037fe383c8d0009a6718e68e87577
Jo-Neto <jcb.oliveira.neto@gmail.com>
2022-09-21 16:33:27 -0300

    kkkk

--- backend-socket/objects/session-object.js
+++ backend-socket/objects/session-object.js
@@ -79,26 +79,6 @@ module.exports = class SessionObject {
                                 msgContent: { update: 'gameEnd', finalData: this.game }
                             }));
                         }
-                        if (Session.currentTurn > 0) {
-                            if (playerWs.aID + Session.currentTurn < Session.activeSockets.length) {
-                                /*            console.log('if')
-                                              console.log(typeof playerWs.aID);
-                                              console.log(typeof Session.currentTurn);
-                                              console.log(playerWs.aID + Session.currentTurn);
-                                              console.log('index 2 ='+ Session.currentTurn); */
-                                Session.game[Number(playerWs.aID + Session.currentTurn)][Number(Session.currentTurn)] = data.msgContent;
-                            }
-                            else {
-                                /*          console.log('else')
-                                            console.log(typeof playerWs.aID);
-                                            console.log(typeof Session.currentTurn);
-                                            console.log(typeof Session.activeSockets.length);
-                                            console.log(playerWs.aID + Session.currentTurn - Session.activeSockets.length);
-                                            console.log(typeof (playerWs.aID + Session.currentTurn - Session.activeSockets.length));
-                                            console.log('index 2 ='+ Session.currentTurn); */
-                                Session.game[Number(playerWs.aID + Session.currentTurn - Session.activeSockets.length)][Number(Session.currentTurn)] = data.msgContent;
-                            }
-                        }
                     });
 
                     this.saveOnDB(false);
