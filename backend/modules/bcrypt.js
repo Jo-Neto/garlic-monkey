f7040e0d9a3e13ac087d66c73df942b22a37a744
Neto <91321255+Jo-Neto@users.noreply.github.com>
2022-09-20 21:06:06 -0300

    Delete bcrypt.js

--- backend/modules/bcrypt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    hashPwd: ( password ) => {
-      const saltRounds = 10;
-      return bcrypt.hashSync(password, saltRounds);
-    },
-    comparePwd: ( password, hash ) => {
-      return bcrypt.compareSync(password, hash);
-    }
-};
