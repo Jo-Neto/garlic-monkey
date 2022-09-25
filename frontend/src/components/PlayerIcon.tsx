70612da2f07e73bb6e38908e75bcb38830973d55
tiagooogomes <tiagooogomes@gmail.com>
2022-09-25 13:33:34 -0300

    add final screen

--- frontend/src/components/PlayerIcon.tsx
+++ frontend/src/components/PlayerIcon.tsx
@@ -1,12 +1,13 @@
 interface PlayerIconProps {
+  foco?: boolean;
   nick: string;
   photo: string;
   player_id: number;
 }
 
-export function PlayerIcon({ nick, photo, player_id }: PlayerIconProps) {
+export function PlayerIcon({ nick, photo, player_id, foco }: PlayerIconProps) {
   return (
-    <div className="h-[2.2rem] w-[12rem] flex flex-row rounded-l-full bg-white/50 rounded-r-md items-center" id={String(player_id)}>
+    <div className={`h-[2.2rem] w-[12rem] flex flex-row rounded-l-full bg-white/50 rounded-r-md items-center ${foco && '!bg-black'}`} id={String(player_id)}>
       <div className="mx-1">
         <img className="bg-white rounded-full" src={'/assets/icons/user.png'} width={40} height={40} />
       </div>
